128b8643047097a1b59950bd98c946e516605b31
ChrisRDavila <crossdavila@gmail.com>
2023-06-27 11:29:31 -0700

    no button wont work but will go throuh all questions change states and then toggle between entry form and ticket list once completed
    Co-authored-by: Laura Hope <lauramhope@users.noreply.github.com>
    Co-authored-by: Qian Li <nancyliqn91@users.noreply.github.com>

---
+++ src/components/DebuggingLesson.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// function DebuggingLesson(){
+//   return (
+//     <React.Fragment>
+//       <h3>Have you gone through all the steps on the Learn How to Program debugging lesson?</h3>
+//       <button>Yes</button>
+//     </React.Fragment>
+//   );
+// }
+
+// export default DebuggingLesson;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,33 +1,86 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import DebuggingLesson from './DebuggingLesson';
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      goDebug:false,
+      askedForHelp: false,
+      spentTimeDocumenting: false,
       formVisibleOnPage: false
     };
-    this.handleClick = this.handleClick.bind(this); //for binding without => scoping
+    // this.handleClick = this.handleClick.bind(this); 
+    // this.handleHelp = this.handleHelp.bind(this);
+    // this.handleDocumenting = this.handleDocumenting.bind(this);
   }
 
+
+  handleDebug = () => {
+    this.setState(prevState => ({
+      goDebug: !prevState.goDebug
+    }));
+  }
+
+  handleHelp = () => {
+    this.setState(prevState => ({
+      askedForHelp: !prevState.askedForHelp
+    }));
+  }
+
+  handleDocumenting = () => {
+    this.setState(prevState => ({
+      spentTimeDocumenting: !prevState.spentTimeDocumenting
+    }));
+  }
+  
   handleClick = () => {
     this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
     }));
   }
-
+  
   render(){
     let currentlyVisibleState = null;
-    let buttonText = null; // new code
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List"; // new code
+    let buttonText = null; 
+    
+    if (!this.state.goDebug){
+      currentlyVisibleState = (
+        <React.Fragment>
+          <h1>Have you gone through the steps in LHTP lesson?</h1>
+          <button onClick={this.handleDebug}>Yes</button>
+        </React.Fragment>
+      );
+      buttonText = "No";
+    } 
+    else if (!this.state.askedForHelp) {
+      currentlyVisibleState = (
+        <React.Fragment>
+          <h1>Have you asked another pair for help?</h1>
+          <button onClick={this.handleHelp}>Yes</button>
+        </React.Fragment>
+      );
+      buttonText = "No";
+    } 
+    else if (!this.state.spentTimeDocumenting) {
+      currentlyVisibleState = (
+        <React.Fragment>
+          <h1>Have you spent 15 min going through the problem and documenting every step?</h1>
+          <button onClick={this.handleDocumenting}>Yes</button>
+        </React.Fragment>
+      );
+      buttonText = "No";        
+    } else if (!this.state.formVisibleOnPage) {
+      currentlyVisibleState = <TicketList />
+      buttonText = "Add Ticket";
     } else {
-      currentlyVisibleState = <TicketList />;
-      buttonText = "Add Ticket"; // new code
+      currentlyVisibleState = <NewTicketForm />;
+      buttonText = "Return to Ticket List";
     }
+
     return (
       <React.Fragment>
         {currentlyVisibleState}
@@ -38,4 +91,4 @@ class TicketControl extends React.Component {
 
 }
 
-export default TicketControl;
+export default TicketControl; 
